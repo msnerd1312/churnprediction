27c8385f5066fb9e227241dd896546a5c50abd56
msnerd1312 <83843924+msnerd1312@users.noreply.github.com>
2024-01-19 09:22:30 -0600

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+snowflake_conn_prop = {
+   "account": "rvb64119.us-east-1",
+   "user": "msenthil_demo",
+   "password": "Demo2023$$",
+   "role": "ACCOUNTADMIN",
+   "database": "CHURN_DB",
+   "schema": "CHURN_SCHEMA",
+   "warehouse": "CHURN_L",
+}
+
+# Snowflake Account Identifiers
+# https://docs.snowflake.com/en/user-guide/admin-account-identifier.html#account-identifier-formats-by-cloud-platform-and-region
+#   "account": "sfpscogs-scs",
+#   "user": "msenthilkumar",
+#   "password": "SnowflakePro2023!",
+#   "role": "SCS_ROLE",
+#   "database": "MALINIS",
+#   "schema": "DATASOURCE",
+#   "warehouse": "WH_XS",
